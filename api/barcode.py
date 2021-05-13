30e837c102801b2720bc9718e0982dedc5b980bb
Josh <Joshuscurtis@gmail.com>
2021-05-13 22:57:41 +0100

    printpath

--- api/barcode.py
+++ api/barcode.py
@@ -35,6 +35,7 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
 
         getBarcode("YQBY7I55SB")
+        print(self.path)
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
