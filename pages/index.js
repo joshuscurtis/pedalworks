cf3166e13a4d88fb450b1288d6da4194fc845582
Josh <Joshuscurtis@gmail.com>
2021-05-15 00:50:53 +0100

    api

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ function Barcode() {
   if (!data) return <a className={styles.card}><h3>Loading...</h3><input type="text" name="sku" onKeyDown={handleKeyDown}/></a>;
   
   return (
-    <div>
+    <div className={styles.container}>
     <a className={styles.card}>
       <h3>Barcode Hunter &rarr;</h3>
       <input type="text" name="sku" onKeyDown={handleKeyDown}     />
