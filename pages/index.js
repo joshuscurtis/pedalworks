f1bb31952c72e9a591834abbf5891bbfac5e6509
Josh <Joshuscurtis@gmail.com>
2021-05-14 23:14:51 +0100

    fix

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         {data.title}
         {data.barcode}
-        {stringify(data)
+        {stringify(data)}
 }
 
       </Head>
