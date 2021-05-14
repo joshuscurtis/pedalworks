a1f38bfb46991e5ad66af1f5483a16b2ce3f547f
Josh <Joshuscurtis@gmail.com>
2021-05-14 23:02:33 +0100

    fix

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,6 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-          {data}
         </h1>
 
         <p className={styles.description}>
