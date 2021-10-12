5313af2767cacbb5a5bb74429779f69f2d17f6ca
joshuscurtis <joshuscurtis@gmail.com>
2021-10-12 15:41:04 +0100

    Update bible.py

--- api/bible.py
+++ api/bible.py
@@ -3,28 +3,32 @@ import re
 
 from bs4 import BeautifulSoup
 
-url = 'https://www.bible.com/en-GB/verse-of-the-day'
+def verseGrab:
+    url = 'https://www.bible.com/en-GB/verse-of-the-day'
+    headers = {
+        'User-Agent': 'My User Agent 1.0',
+        'From': 'youremail@domain.com'  # This is another valid field
+    }
+    response = requests.get(url, headers=headers)
+    title_search = re.search('<meta property=og:description content="(((.|\n)*))"><meta property=tw', str(response.content), re.IGNORECASE)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    todaysVerseContent = soup.find_all('p', class_='yv-gray50 mt0 mb2')[0].text.replace("\n", " ")
+    todaysVerse = soup.find_all('p', class_='usfm fw7 mt0 mb0 yv-gray25 f7 ttu')[0].text.replace("\n", " ")
+    print(todaysVerseContent)
+    print(todaysVerse)
+    verse = {}
+    verse['content'] = todaysVerseContent
+    verse['ref'] = todaysVerse
+return verse
 
-headers = {
-    'User-Agent': 'My User Agent 1.0',
-    'From': 'youremail@domain.com'  # This is another valid field
-}
 
-response = requests.get(url, headers=headers)
 
-title_search = re.search('<meta property=og:description content="(((.|\n)*))"><meta property=tw', str(response.content), re.IGNORECASE)
+class handler(BaseHTTPRequestHandler):
 
-soup = BeautifulSoup(response.content, 'html.parser')
-
-todaysVerseContent = soup.find_all('p', class_='yv-gray50 mt0 mb2')[0].text.replace("\n", " ")
-todaysVerse = soup.find_all('p', class_='usfm fw7 mt0 mb0 yv-gray25 f7 ttu')[0].text.replace("\n", " ")
-
-print(todaysVerseContent)
-print(todaysVerse)
-
-verse = {}
-
-verse['content'] = todaysVerseContent
-verse['ref'] = todaysVerse
-
-print(verse)
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Cache-Control', 's-maxage=5, stale-while-revalidate=2629743')
+        self.end_headers()
+        self.wfile.write(verseGrab.encode())
+        return
