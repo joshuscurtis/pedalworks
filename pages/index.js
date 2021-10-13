27368fdf4830f9c41e533c97b3a15e73d370c332
Josh <Joshuscurtis@gmail.com>
2021-10-13 22:48:51 +0100

    all

--- pages/index.js
+++ pages/index.js
@@ -1,175 +1,20 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import useSWR from 'swr';
-import axios from 'axios';
-import fetch from 'isomorphic-unfetch';
-import { useState, useEffect } from 'react'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import useSWR from "swr";
+import axios from "axios";
+import fetch from "isomorphic-unfetch";
+import { useState, useEffect } from "react";
+import Verse from "../parts/verse";
 
-var data = {"content": "If anyone acknowledges that Jesus is the Son of God, God lives in them and they in God.", "ref": "1 John 4:15 (NIV)"}
-
-
-function Barcode() {
-  const [sku, setSKU] = useState("YQBY7I61SB")
-  const API_URL = 'api/barcode?sku=' + sku;
-  var { data, error } = useSWR(API_URL, fetcher);
-
-  async function fetcher(url) {
-    const res = await fetch(url);
-    const json = await res.json();
-    return json;
-  }
-  
-
-
-
-
-  function handleKeyDown(e) {
-    if (e.key === 'Enter') {
-      setSKU(e.target.value);
-    }
-  }
-
-  if (error) return <a className={styles.card}><h3>Failed to load</h3><input type="text" name="sku" onKeyDown={handleKeyDown}/></a>;
-  if (!data) return <a className={styles.card}><h3>Loading...</h3><input type="text" name="sku" onKeyDown={handleKeyDown}/></a>;
-  
-  return (
-    <a className={styles.card}>
-        <h3>Barcode Hunter &rarr;</h3>
-        <input type="text" name="sku" onKeyDown={handleKeyDown} />
-        <a className={styles.card}>
-          <h3>Data</h3>
-          <p><strong>Name: </strong> {data.title}</p>
-          <p><strong>Barcode: </strong> {data.barcode}</p>
-          <p><strong>SKU: </strong> {data.sku}</p>
-        </a>
-    </a>
-  )
-}
-
-function getVerse() {
-  const API_URL = 'api/bible';
-  var { data, error } = useSWR(API_URL, fetcher);
-
-  async function fetcher(url) {
-    const res = await fetch(url);
-    const json = await res.json();
-
-    return json;
-  }
-}
 
 export default function Home() {
-  var loading = {"content": "If anyone acknowledges that Jesus is the Son of God, God lives in them and they in God.", "ref": "1 John 4:15 (NIV)"}
-  const [updateData, setUpdateData] = useState(loading);
-  const [verseArray, setVerseArray] = useState(loading.content.split(' '));
-  const [input, setInput] = useState("")
-  const [hide, setHide] = useState(false)
+              return(
+                <div className={styles.main}>
+                  <h1>Memory Verse of the Day</h1>
+                  <div className={styles.card}>
+                    <Verse></Verse>
+                  </div>
+              </div>
 
-  useEffect(() => {
-    console.log("useEffect");
-    async function fetchData() {
-      const request2 = await axios.get('api/bible');
-      setUpdateData(request2.data);
-      setVerseArray(request2.data.content.split(' '))
-      console.log(verseArray);
-      console.log(updateData);
-      return request2;
-    }
-    fetchData();
-  }, []);
-  
-
-  if (updateData !== 'loading' && updateData.content == input) return (
-    <div> 
-    {hide &&
-    verseArray.map(word => <a>{word + " "}</a>)}      <p></p>
-      <textarea onChange={(e) => setInput(e.target.value)}></textarea>
-      <p> {updateData.ref}  </p>
-      <h1>You got it right!</h1>
-      <button onClick={(e) => setHide(!hide)}>
-        {hide &&
-         "Hide"}
-         {!hide &&
-         "Unhide"}
-      </button>
-    </div>
-    
-  )
-  if (updateData !== 'loading') return (
-    <div> 
-      
-    
-    
-    {hide &&
-    verseArray.map(word => <a>{word + " "}</a>)}
-
-      <p></p>
-      <textarea onChange={(e) => setInput(e.target.value)}>
-        Hello there, this is some text in a text area
-      </textarea>
-      <p> {updateData.ref}  </p>
-      <button onClick={(e) => setHide(!hide)}>
-        {hide &&
-         "Hide"}
-         {!hide &&
-         "Unhide"}
-      </button>
-    </div>
-    
-  )
-
-  if (updateData == 'loading') return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <Barcode/>
-
-      
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+              );
 }
