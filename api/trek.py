db0a4f5d3158c475991f39178464670baaac6f4a
Josh <Joshuscurtis@gmail.com>
2022-01-13 16:10:11 +0000

    trek

--- api/trek.py
+++ api/trek.py
@@ -71,16 +71,36 @@ def finder(pn):
     return "Josh"
 
 
+def getBarcode(part_number):
+    url = "https://www.rutlandcycling.com/facetresults.aspx?Term="+part_number
+    regex = "var universal_pageType = \'product\'; var universal_product =([^|]+);<"
+
+    print(url)
+    headers1 = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+        }
+
+    raw = requests.get(url, headers=headers1)
+    soup = BeautifulSoup(raw.content, "html.parser")
+    title = soup.title.text
+    body = raw.content.decode('utf-8')
+    x = re.search(regex, body)
+    data = x.group(1)
+    data = json.loads(data)
+
+    attributes = data["attributes"]
+
+    for attribute in attributes:
+        if (attribute['code'] == part_number):
+            return (attribute['barcode'])
 
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-
-        finder(32564)
+        finder("35677")
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
-        self.send_header('Cache-Control', 's-maxage=5, stale-while-revalidate=2629743')
         self.end_headers()
-        self.wfile.write(finder(32564).encode())
+        self.wfile.write(finder("35677").encode())
         return
